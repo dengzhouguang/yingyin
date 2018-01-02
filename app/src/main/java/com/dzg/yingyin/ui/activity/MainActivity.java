3386a7d16188c7310d8574896b77543cf8d80ab1
dengzhouguang <979611780@qq.com>
2018-01-02 23:39:18 +0800

    commit

--- app/src/main/java/com/dzg/yingyin/ui/activity/MainActivity.java
+++ app/src/main/java/com/dzg/yingyin/ui/activity/MainActivity.java
@@ -1,8 +1,13 @@
 package com.dzg.yingyin.ui.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,7 +16,6 @@ import com.dzg.yingyin.R;
 import com.dzg.yingyin.ui.fragment.LocalFragment;
 import com.dzg.yingyin.ui.fragment.SettingFragment;
 import com.dzg.yingyin.ui.fragment.ShareFragment;
-import com.dzg.yingyin.util.ExeCommand;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,14 +31,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         init();
-        ExeCommand exeCommand=new ExeCommand();
-        exeCommand.run("dumpsys battery",2000);
-        android.util.Log.e("以后的以后",exeCommand.getResult()+"没结果？");
     }
 
     private void init() {
         getSupportFragmentManager().beginTransaction().replace(R.id.frame,new LocalFragment()).commit();
         mBNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            requestPermissions();
+        }
     }
 
 
@@ -60,14 +64,49 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        /*MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.actionbar_menu, menu);*/
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
+    }
+
+    public void requestPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED||ContextCompat.checkSelfPermission(this,
+                Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS)
+                != PackageManager.PERMISSION_GRANTED||ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)||ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)||ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS},1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        if (requestCode == 1)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+
+            } else
+            {
+                finish();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
 }
